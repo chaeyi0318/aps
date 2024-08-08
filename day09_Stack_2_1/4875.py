0267c0b7b190c2563e8e8114f6083d0df620b622
Chaeyi <chaeyi0318@gmail.com>
2024-08-09 01:06:23 +0900

    ,

--- day09_Stack_2_1/4875.py
+++ day09_Stack_2_1/4875.py
@@ -2,36 +2,70 @@ import sys
 
 sys.stdin = open('4875_input.txt')
 
-dxy = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-
 T = int(input())
 
-def find_start(maze):
 
+def dfs(i, j, N):
+    dxy = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    visited[i][j] = 1
+
+    if maze[i][j] == 3:
+        return 1
+
+    for x in range(len(dxy)):
+        point_x = i + dxy[x][0]
+        point_y = j + dxy[x][1]
+
+        if 0 <= point_x < N and 0 <= point_y < N and maze[point_x][point_y] != 1 and visited[point_x][point_y] == 0:
+            if dfs(point_x, point_y, N):
+                return 1
+    return 0
 
 
 for t in range(1, T + 1):
     N = int(input())
+
     maze = [list(map(int, input())) for _ in range(N)]
-    start_i, start_j = find_start(maze)
+    visited = [[0] * N for _ in range(N)]
+
+    start_x, start_y = 0, 0
 
     for i in range(len(maze)):
         for j in range(len(maze[i])):
             if maze[i][j] == 2:
-                start[0] = i
-                start[1] = j
+                start_x = i
+                start_y = j
+                break
 
-    stack = [start]
+    print(f'#{t}', dfs(start_x, start_y, N))
 
-    # while stack:
-    for x in range(len(dxy)):
-        point_x = start[0] + dxy[x][0]
-        point_y = start[1] + dxy[x][1]
+    # print(start_x, start_y)
 
-        if 0 <= point_x < len(maze) and 0 <= point_y < len(maze):
-            print(maze[point_x][point_y])
-
-    print()
-
-            # stack.append([point_x, point_y])
-
+# T = int(input())
+#
+# def dfs(i, j, N):
+#     dxy = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+#
+#     for x in range(len(dxy)):
+#         point_x = i + dxy[x][0]
+#         point_y = j + dxy[x][1]
+#
+#         if 0 <= point_x < len(maze) and 0 <= point_y < len(maze):
+#             print(maze[point_x][point_y])
+#
+#
+# def find_start(maze):
+#     for i in range(len(maze)):
+#         for j in range(len(maze[i])):
+#             if maze[i][j] == 2:
+#                 return i, j
+#
+# for t in range(1, T + 1):
+#     N = int(input())
+#     maze = [list(map(int, input())) for _ in range(N)]
+#     visited = [[0] * N for _ in range(N)]
+#
+#     start_i, start_j = find_start(maze)
+#
+#     print(dfs)
+#
