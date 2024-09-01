341cffd75a23f80e4fdb1a1c655b79e538647c10
Chaeyi <chaeyi0318@gmail.com>
2024-09-02 00:54:19 +0900

    update

--- prac.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.stdin = open('prac_input.txt')
-
-# V : 정점 개수 / E : 간선 수
-V, E = map(int, input().split())
--- 그래프 탐색.py
+++
@@ -1,9 +0,0 @@
-for i in range(1):
-    V, E = map(int, input().split())
-    arr = list(map(int, input().split()))
-
-    matrix = [0] * V
-
-
-
-    print(arr)
