6b1841cca59bf235614fc39729192609d551ce76
Chaeyi <chaeyi0318@gmail.com>
2024-09-23 00:17:50 +0900

    update

---
+++ pro/주식가격.py
@@ -0,0 +1,18 @@
+def solution(prices):
+    answer = []
+
+    for i in range(len(prices)):
+        cnt = 0
+
+        for j in range(i + 1, len(prices)):
+            cnt += 1
+
+            if prices[i] > prices[j]:
+                break
+
+        answer.append(cnt)
+
+    return answer
+
+prices = [1, 2, 3, 2, 3]
+print(solution(prices))
